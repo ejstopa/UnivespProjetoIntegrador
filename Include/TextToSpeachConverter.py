e13417182e2ff16a3edc4857849e2d835ed9534e
ejstopa <106110084+ejstopa@users.noreply.github.com>
2022-06-22 00:41:33 -0300

    Update TextToSpeachConverter.py

--- Include/TextToSpeachConverter.py
+++ Include/TextToSpeachConverter.py
@@ -7,7 +7,7 @@ class TextToSpeachConverterPyttsx3:
     
     def ConvertAndPlay(self, textToConvert : str):
         
-        engine = pyttsx3.init("espeak") 
+        engine = pyttsx3.init() 
         engine.say(textToConvert)
 
         engine.runAndWait()

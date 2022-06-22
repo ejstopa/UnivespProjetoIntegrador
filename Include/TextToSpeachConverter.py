fc32a620febd008fb9c15b8861cdd7c3256fc460
ejstopa <106110084+ejstopa@users.noreply.github.com>
2022-06-22 19:35:46 -0300

    Update TextToSpeachConverter.py

--- Include/TextToSpeachConverter.py
+++ Include/TextToSpeachConverter.py
@@ -7,7 +7,7 @@ class TextToSpeachConverterPyttsx3:
     
     def ConvertAndPlay(self, textToConvert : str):
         
-        engine = pyttsx3.init() 
+        engine = pyttsx3.init("sapi5") 
         engine.say(textToConvert)
 
         engine.runAndWait()

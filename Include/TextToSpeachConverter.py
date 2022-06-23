3d85f7f58091ee23853dfaa7df61349eda4f9fbc
ejstopa <106110084+ejstopa@users.noreply.github.com>
2022-06-22 23:00:31 -0300

    updates

--- Include/TextToSpeachConverter.py
+++ Include/TextToSpeachConverter.py
@@ -4,6 +4,8 @@ import pyttsx3
 from gtts import gTTS 
 from playsound import playsound
 from pathlib import Path
+import pygame
+
  
         
 class TextToSpeachConverterPyttsx3:
@@ -22,11 +24,30 @@ class TextToSpeachConverterGtts:
         
         BASE_DIR = Path(__file__).resolve().parent.parent
         staticRoot = os.path.join(BASE_DIR,'staticfiles')
-        outputFileName = os.path.join(staticRoot,'output.mp3')
+        # outputFileName = os.path.join(staticRoot,'output.mp3')
+        outputFileName = BytesIO()
         myObj = gTTS(text=textToConvert, lang= str(language), slow=False)   
-        myObj.save(outputFileName)       
+        # myObj.save(outputFileName) 
+        myObj.write_to_fp(outputFileName)   
+        outputFileName.seek(0)
 
-        playsound(outputFileName)
-        os.remove(outputFileName)
+        # playsound(outputFileName)
+
+        pygame.init()
+        pygame.mixer.init()
+    
+        pygame.mixer.music.load(outputFileName, 'mp3')
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
+
+        # os.remove(outputFileName)
+
+
+    
+
+
 
         
